251b61018795cbb24f54ccf8627923d9d8b5761c
luren549 <228224905@qq.com>
2020-11-28 13:28:13 +0800

    更新短网址接口

--- js/stui_block.js
+++ js/stui_block.js
@@ -248,32 +248,35 @@ var stui = {
 			if(copyshort==1){
 				if($(".copylink").length){
 					$.ajax({ 
-						type : 'GET',  
-				        url : 'https://api.weibo.com/2/short_url/shorten.json?source=2849184197&url_long='+encodeURIComponent(stui.browser.url),
-				        dataType : 'JSONP',   
+						type : 'post',  
+						url: 'https://www.gourls.net/dwz.php',
+						dataType: 'JSON',
+						data: {
+							longurl:stui.browser.url
+						},  
 				        success : function(r) {
-				        	url_short = r.data.urls[0].url_short;	
+				        	url_short = "全网vip高清影视院线大片等，免费在线观看："+r.ae_url;	
 				        	var clipboard = new Clipboard('.copylink', {
 								text: function() {									
 									return url_short;
 								}
 							});
 							clipboard.on('success', function(e) {
-								alert("地址复制成功，赶快分享给小伙伴吧！");
+								alert("地址复制成功，赶快分享给小伙伴，该视频即可加速播放哦！");
 							});
 			        	} 		        	
 				    });
 				}
 			} else {
 				if($(".copylink").length){
-					var url_short = stui.browser.url;	
+					url_short = "全网vip高清影视院线大片等，免费在线观看："+stui.browser.url;	
 					var clipboard = new Clipboard('.copylink', {
 						text: function() {									
 							return url_short;
 						}
 					});
 					clipboard.on('success', function(e) {
-						alert("地址复制成功，赶快分享给小伙伴吧！");
+						alert("地址复制成功，赶快分享给小伙伴，该视频即可加速播放哦！");
 					});
 				}
 			}
